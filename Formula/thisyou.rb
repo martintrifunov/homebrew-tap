17ce6b7bec8885fb15e6c1e34db23864173a7af1
Martin Trifunov <martintrifunov@proton.me>
2026-08-26 10:49:55 +0200

    update thisyou to 0.2.0

--- Formula/thisyou.rb
+++ Formula/thisyou.rb
@@ -1,8 +1,8 @@
 class Thisyou < Formula
   desc "Terminal UI for exploring archived Reddit histories"
   homepage "https://github.com/martintrifunov/thisyou"
-  url "https://github.com/martintrifunov/thisyou/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "7e2d2ecf1617785cc30f21be2171f335945a35fc9c615204055f147bdbce4ee2"
+  url "https://github.com/martintrifunov/thisyou/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "bf88f8b00018c4fbe157c696b21261423d7133b575d6842175e685f6e6b5b5a4"
   license "MIT"
   head "https://github.com/martintrifunov/thisyou.git", branch: "main"
 
